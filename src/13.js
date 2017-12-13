cacd82b08b025cfef6cbb6e9e76c7c3ca949a7e6
Niko Pavlica <n.pavlica@sportradar.com>
2017-12-13 09:09:05 +0100

    13 with better check

--- src/13.js
+++ src/13.js
@@ -25,82 +25,26 @@ function sanitize(aa) {
         );
 }
 
-function guaranteedCollision(delayedSeconds) {
-    // based on my data... should be more generic :)
-    return delayedSeconds % 2 === 0;
-}
-
-function moveScanners(scannerLocations, scannerDirections, levelLimits, levels) {
-    for (let i = 0, l = levels.length; i <= l; i++) {
-        const level = levels[i];
-        const loc = (scannerLocations[level] += scannerDirections[level]);
-        if (loc >= levelLimits[level] || loc <= 1) {
-            scannerDirections[level] *= -1;
-        }
-    }
-}
-
 function doMagic(input) {
     // it's a kind of maagiiiic
-    const levelLimits = {};
-    const scannerLocations = {};
-    const scannerDirections = {};
     const scannerLevels = [];
-    let maxLevel = 0;
     input.forEach(([level, range]) => {
-        levelLimits[level] = range;
-        scannerLevels.push(level);
-        scannerLocations[level] = 1;
-        scannerDirections[level] = 1;
-        if (level > maxLevel) maxLevel = level;
+        scannerLevels[level] = range;
     });
 
-    // let delayedSeconds = 60443;
     let delayedSeconds = 0;
-    let skipUntil = 0;
-    let d;
-    do {
-        if (delayedSeconds >= skipUntil) {
-            d = getsCaught(input, 0, levelLimits, scannerLocations, scannerDirections, scannerLevels, maxLevel);
+    while (true) {
+        if (!getsCaught(scannerLevels, delayedSeconds)) {
+            break;
         }
-        if (d !== false) {
-            console.log("not good at ", delayedSeconds, "caught at ", d);
-            do {
-                delayedSeconds++;
-                moveScanners(scannerLocations, scannerDirections, levelLimits, scannerLevels);
-            } while (guaranteedCollision(delayedSeconds));
-        }
-    } while (d !== false);
+        delayedSeconds++;
+    }
 
-    console.log(delayedSeconds + 1);
+    console.log(delayedSeconds);
 }
 
-function getsCaught(input, delayedSeconds, levelLimits, inscannerLocations, inscannerDirections, scannerLevels, maxLevel) {
-    const scannerLocations = { ...inscannerLocations };
-    const scannerDirections = { ...inscannerDirections };
-    let damage = 0;
-    let packetLoc = -1;
-    let maxDamage = 0;
-
-    let steps = 0;
-    do {
-        if (steps > delayedSeconds) {
-            // move packet
-            packetLoc++;
-        }
-        steps++;
-
-        // check if caught
-        if (scannerLocations[packetLoc] === 1) {
-            return packetLoc;
-        }
-
-        // move scanners
-        moveScanners(scannerLocations, scannerDirections, levelLimits, scannerLevels);
-        // console.log(JSON.stringify(scannerLocations));
-    } while (packetLoc < maxLevel);
-
-    return false;
+function getsCaught(scannerLevels, delayedSeconds) {
+    return scannerLevels.some((limit, packetLoc) => limit && (packetLoc + delayedSeconds) % (2 * limit - 2) === 0);
 }
 
 run();
